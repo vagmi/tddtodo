eee535de69fc5f00ff23d7cb78722b8fc1208576
vagmi <vagmi.mudumbai@gmail.com>
2012-03-15 10:10:47 +0530

    add spec and impl for display method

--- lib/models/item.rb
+++ lib/models/item.rb
@@ -1,5 +1,12 @@
 module Models
   class Item
     attr_accessor :content, :status
+    def display
+      if(@status)
+        "#{@content} #done"
+      else
+        @content
+      end
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,4 +7,26 @@ describe Models::Item do
   it "should have status" do
     subject.respond_to?(:status).should be_true
   end
+  describe "#display" do
+    before do
+      @item = Models::Item.new
+      @item.content = "My Content"
+    end
+    context "if status is true " do
+      before do
+        @item.status = true
+      end
+      it "should display with the done tag" do
+        @item.display.should == "#{@item.content} #done"
+      end
+    end
+    context "if status is false" do
+      before do
+        @item.status = false
+      end
+      it "should display without the done tag" do
+        @item.display.should == @item.content
+      end
+    end
+  end
 end
